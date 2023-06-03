46da92395eb16d14da8774525ee46dc9b3e7731e
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2023-06-03 11:17:38 -0300

    Finalizei Aula 14 Exercicios

---
+++ Aula 06/maratona-java-jiraya/src/br/com/altsystem/maratonajavajiraya/introducao/Aula14TiposPrimitivosExercicio.java
@@ -0,0 +1,18 @@
+package br.com.altsystem.maratonajavajiraya.introducao;
+
+import java.util.Date;
+
+public class Aula14TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Altamir Caetano de Oliveira";
+        String endereco = "Quadra 5 - Casa 02";
+        Double salario = 7886.77;
+        String dtRecebimentoSalario = "10/05/1967";
+
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + "\n" + "confirmo que recebi o salário de R$" + salario + ", na data de: " + dtRecebimentoSalario);
+
+    }
+
+}
